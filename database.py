dcf7603d57bf52c13df3da1f0ab314132c6bf160
MRSAEED88 <39449961+MRSAEED88@users.noreply.github.com>
2025-11-11 22:44:11 +0300

    Update database.py

--- database.py
+++ database.py
@@ -1,8 +1,25 @@
 import sqlite3 
 
-con_user = sqlite3.connect('User.dp')
-# Creat  users table
+
+
+
+con_user = sqlite3.connect('User.db')
+# Create users table
 info = con_user.cursor()
-info.execute("CREATE TABLE users(user INTEGAR,name TEXT,emal TEXT,membership TEXT)")
-con_user.commit
+info.execute("CREATE TABLE IF NOT EXISTS users(ID INTEGER PRIMARY KEY,name TEXT,email TEXT ,membership TEXT)")
+
+con_user.commit()
 con_user.close()
+
+
+class adduser:
+    def __init__(self,userinfo, con='User.db'):
+        self.con= sqlite3.connect(con)
+        self.info= self.con.cursor()
+        self.userinfo= userinfo
+    def insertData(self):
+        self.info.execute("INSERT INTO users Values(?,?,?,?)", self.userinfo)
+        self.con.commit()
+        self.con.close()
+        return None
+    
