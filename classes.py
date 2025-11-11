f3a8801f032e8cc019bc66d35bda4cee40ff522d
MRSAEED88 <39449961+MRSAEED88@users.noreply.github.com>
2025-11-11 22:45:15 +0300

    Update classes.py

--- classes.py
+++ classes.py
@@ -1,12 +1,20 @@
+import Users_db
+
 class User :
-    def __init__(self,user_id,name,emal,membership):
+    def __init__(self,user_id,name,email,membership):
         self.user_id = user_id
         self.name = name
-        self.emal = email
+        self.email = email
         self.membership = membership
+    def store_data(self):
+        user_info=Users_db.adduser((self.user_id,self.name,self.email,self.membership))
+        user_info.insertData()
+
 
     def is_admin(self):
         return self.membership == "Admin"
     def is_student(self):
         return self.membership == "student"
         pass
+
+
