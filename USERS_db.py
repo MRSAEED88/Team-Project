9d0b1d2685fe551224237d532ee8a21bba09af9b
aalshthewani0001-dev <aalshthewani0001@stu.kau.edu.sa>
2025-11-12 17:08:17 +0300

    Update USERS_db.py

--- USERS_db.py
+++ USERS_db.py
@@ -21,3 +21,26 @@ class add_db:
         self.con.close()
         return None
     
+import random
+
+class add_student:
+    def __init__(self):
+        self.User = []
+    
+    def createstudent(self,number = 10):
+        names = ["Abdulilah", "Saeed", "Sulaiman", "Alaa", "Mohtadi", "Baraa",
+                 "Faisal", "Omar", "Khalid", "Ahmed", "Rayan", "Hassan"]
+        programs = ["Power and Machines", "Communication and Electronics", "Computer",
+                       "Biomedecal"]
+        
+        for i in range(1,11):
+            name = random.choice(names)
+            program = random.choice(programs)
+            level = random.randint(1,4)
+            ID = random.randint(2400000, 2499999)
+            passw =random.ramdint(1234567,2345678) 
+
+            email = (f"{name.lower()}{i} @kau.edu.stu.com")
+            student = student(ID, name, email, program, level)
+            self.User.append(student)
+        pass
