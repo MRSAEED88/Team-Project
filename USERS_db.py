b45a194899b9f96bffc7ba0e7380fbc52cd75b3f
aalshthewani0001-dev <aalshthewani0001@stu.kau.edu.sa>
2025-11-12 16:59:16 +0300

    Rename class from adduser to add_db

--- USERS_db.py
+++ USERS_db.py
@@ -10,14 +10,14 @@ con_user.commit()
 con_user.close()
 
 
-# class adduser:
-#     def __init__(self,userinfo, con='User.db'):
-#         self.con= sqlite3.connect(con)
-#         self.info= self.con.cursor()
-#         self.userinfo= userinfo
-#     def insertData(self):
-#         self.info.execute("INSERT INTO users Values(?,?,?,?)", self.userinfo)
-#         self.con.commit()
-#         self.con.close()
-#         return None
+class add_db:
+    def __init__(self,userinfo, con='User.db'):
+        self.con= sqlite3.connect(con)
+        self.info= self.con.cursor()
+        self.userinfo= userinfo
+    def insertData(self):
+        self.info.execute("INSERT INTO users Values(?,?,?,?)", self.userinfo)
+        self.con.commit()
+        self.con.close()
+        return None
     
