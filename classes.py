8b86908dd9269c531a9a957c12a5fcd7a01a2e7c
aalshthewani0001-dev <aalshthewani0001@stu.kau.edu.sa>
2025-11-12 17:01:16 +0300

    Update classes.py

--- classes.py
+++ classes.py
@@ -7,7 +7,7 @@ class User :
         self.email = email
         self.membership = membership
     def store_data(self):
-        user_info=Users_db.adduser((self.user_id,self.name,self.email,self.membership))
+        user_info=Users_db.add_db((self.user_id,self.name,self.email,self.membership))
         user_info.insertData()
 
 
