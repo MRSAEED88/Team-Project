d7a3bca44e06d371be5ef6c5492c2a0e45f88a1b
aalshthewani0001-dev <aalshthewani0001@stu.kau.edu.sa>
2025-11-15 18:50:12 +0300

    Update users_db.py

--- users_db.py
+++ users_db.py
@@ -1,5 +1,4 @@
 import sqlite3 
-# import random
  
 
 # creat a file 
@@ -9,14 +8,14 @@ info = con_user.cursor()
 info.execute("CREATE TABLE IF NOT EXISTS users(ID INTEGER UNIQUE,name TEXT," 
 "email TEXT,passWord TEXT,membership TEXT)")  
 
-#creat student table
+# CREAT TABLE FOR STUDENTS
 info.execute("CREATE TABLE IF NOT EXISTS students(ID INTEGER UNIQUE,name TEXT," 
 "email TEXT,program TEXT,level INTEGER,hours_completed INTEGER," 
 "hours_remaining INTEGER)")
 
 
 
-
+# Here we would like to tcheck from student's table
 # data = info.execute("SELECT * FROM students")
 # data = data.fetchall()
 # for row in data:
@@ -30,7 +29,7 @@ con_user.close()
 
 class user_db:
     def __init__(self,userinfo):
-        self.userinfo= userinfo
+        self.userinfo = userinfo
     def insertData(self):
         info.execute("INSERT INTO users VALUES (?, ?, ?, ?, ?)", self.userinfo)
          
@@ -94,29 +93,3 @@ class serach:
 
 
 
-# class add_student:
-#     def __init__(self):
-#         self.User = []
-    
-#     def createstudent(self,number = 10):
-#         names = ["Abdulilah", "Saeed", "Sulaiman", "Alaa", "Mohtadi", "Baraa",
-#                  "Faisal", "Omar", "Khalid", "Ahmed", "Rayan", "Hassan"]
-#         programs = ["Power and Machines", "Communication and Electronics", "Computer",
-#                        "Biomedecal"]
-        
-#         for i in range(1,number):
-#             name = random.choice(names)
-#             program = random.choice(programs)
-#             level = random.randint(1,4)
-#             ID = random.randint(2400000, 2499999)
-#             passw =random.randint(1234567,2345678)
-            
-#             email = (f"{name.lower()}{i}@kau.edu.stu.com")
-#             user=add_users((ID,name,email,passw,"student")) 
-#             user.insertData()
-
-#             student =classes.Student(ID, name, email, program, level)
-#         #     self.User.append(student)
-#         # pass
-# s= add_student()
-# s.createstudent()
