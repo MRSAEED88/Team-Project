e5ca8c9feae1e80af2b07a89dc2fd0dd283fadcf
Alassiri1 <slimanassiri3@gmail.com>
2025-11-24 01:45:34 +0300

    User.py

---
+++ User.py
@@ -0,0 +1,17 @@
+class User :
+    def __init__(self,user_id : int,name : str,email : str,password : str ,membership : str ):
+        self.user_id = user_id
+        self.name = name
+        self.email = email
+        self.password = password
+        self.membership = membership
+    def store_data(self):
+        user_info=users_db.add_users((self.user_id,self.name,self.email, self.password,self.membership))
+        user_info.insertData()
+
+
+    def is_admin(self):
+        return self.membership == "Admin"
+    def is_student(self):
+        return self.membership == "student"
+        pass
