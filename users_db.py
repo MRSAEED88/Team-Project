032c134418dbcad72a92b9125646260cd24f424c
MRSAEED88 <39449961+MRSAEED88@users.noreply.github.com>
2025-11-28 00:15:20 +0300

    Refactor users_db.py for students table and cleanup
    Updated the database structure and modified queries to select from the students table. Removed commented-out code and TODOs related to course creation.

--- users_db.py
+++ users_db.py
@@ -13,6 +13,8 @@ info.execute("CREATE TABLE IF NOT EXISTS users(ID INTEGER UNIQUE,name TEXT,"
 info.execute("CREATE TABLE IF NOT EXISTS students(ID INTEGER UNIQUE,name TEXT," 
 "email TEXT,program TEXT,level INTEGER,transcript TEXT)")
 
+
+
 #TODO: we should change the table structure to store transcript instead of hours completed and remaining => (DONE!!)
 # but we have to adjust the code accordingly
 
@@ -36,10 +38,10 @@ info.execute("CREATE TABLE IF NOT EXISTS registration(student_id INTEGER,"
 
 # Here if we like to check table we uncomment this part
 
-# data = info.execute("SELECT * FROM courses")
-# data = data.fetchall()
-# for row in data:
-#     print(row)
+data = info.execute("SELECT rowid,* FROM students")
+data = data.fetchall()
+for row in data:
+    print(row)
 
 
 con_user.commit()
@@ -48,20 +50,6 @@ con_user.close()
 
 #TODO: We should work on removing unnecessary classes and try to make the code more efficient
 # (for example merging claseses that do similar things)
-
-# I commented this part becase its duplicated in added_users class below delete if not needed
-
-# class user_db:
-#     def __init__(self,userinfo):
-#         self.userinfo = userinfo
-#     def insertData(self):
-#         info.execute("INSERT INTO users VALUES (?, ?, ?, ?, ?)", self.userinfo)
-         
-
-
-
-
-
 
 
 class student_db:
@@ -88,6 +76,11 @@ class add_users:
         info.execute("INSERT INTO users VALUES (?, ?, ?, ?, ?)", self.userinfo)
         con_user.commit()
         con_user.close()
+#The comment is for future use if we decide to add student through this class
+    # def insertStudent(self):
+    #     self.info.execute("INSERT INTO students VALUES (?, ?, ?, ?, ?, ?, ?)", self.userinfo)
+    #     self.con.commit()
+    #     self.con.close()
 
 #This class is for adding courses to the database
 class courses_db:
@@ -100,11 +93,7 @@ class courses_db:
         con_user.commit()
         con_user.close()
 
-#The comment is for future use if we decide to add student through this class
-    # def insertStudent(self):
-    #     self.info.execute("INSERT INTO students VALUES (?, ?, ?, ?, ?, ?, ?)", self.userinfo)
-    #     self.con.commit()
-    #     self.con.close()
+
 
 # class search:
 #     def __init__(self, parameter, search_by='id'):
@@ -131,11 +120,23 @@ class courses_db:
 #         user_data = info.fetchone()
 #         con_user.close()
 #         return user_data
+
+
+
+
+
+
 class search:
     def __init__(self, parameter, table="users", search_by="id"):
         self.table = table.lower()       # users / students / courses
         self.search_by = search_by.lower()
         self.parameter = parameter
+
+    
+    #TODO: This method should be tested to ensure it works as expected
+  # As you can see here we made a class for creat a courses:
+
+
 
     def fetch(self):
         con_user = sqlite3.connect('User.db')
@@ -184,42 +185,4 @@ class search:
         con_user.close()
         return result
 
-#TODO: This method should be tested to ensure it works as expected
-  # As you can see here we made a class for creat a courses:
 
-
-#TODO: I have transferred the code below to courseFactory.py if you dont need the code you can delete it from here
-
-
-# class CourseFactory:
-#     @staticmethod # => Does not Depend on main clss it is onle order function into the class:
-#     def create_and_save_courses(): # We sved as a list
-#         courses = [
-#             (1, "EE250", "Fundamental of Electrical Circuits", 4, 40, "REQ"), #REQ = I dont know about its req yet.
-#             (2, "EE201", "Python", 3, 40,"REQ"),
-#             (3, "EE311", "Electronics", 4, 35,"REQ"),
-#             (4, "EE300", "Complex", 3, 40,"REQ"),
-#             (5, "EE301", "Circuit I", 3, 40, "EE2250"),
-#             (6, "EE341", "Machines", 3, 35,"EE2250"),
-#         ]
-
-#         # Connect to DB
-#         con = sqlite3.connect("User.db")
-#         cur = con.cursor()
-
-#         # Insert each course
-#         for c in courses:
-#             cur.execute("""
-#                 INSERT OR REPLACE INTO courses(ID, course_code, course_name, credits, capacity, prereq)
-#                 VALUES (?, ?, ?, ?, ?, ?)
-#             """, c)
-#          # c.get_tuple() = (c.course_id, c.course_code, c.course_name, c.credits, c.capacity, c.prereq)
-
-#         con.commit()
-#         con.close()
-
-#         # print("6 Courses added successfully!")
-
-
-
-# CourseFactory.create_and_save_courses()
