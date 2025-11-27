5fdc54e147112a257f2c6545a3c0fed10a504e31
aalshthewani0001-dev <aalshthewani0001@stu.kau.edu.sa>
2025-11-28 00:23:50 +0300

    Update createstudent.py

--- createstudent.py
+++ createstudent.py
@@ -4,22 +4,22 @@ import random
 
 
 class add_student:
-    def __init__(self):
+    def init(self):
         self.User = []
-    
+
     def createstudent(self,number = 10):
         names = ["Abdulilah", "Saeed", "Sulaiman", "Alaa", "Mohtadi", "Baraa",
                  "Faisal", "Omar", "Khalid", "Ahmed", "Rayan", "Hassan"]
         programs = ["Power and Machines", "Communication and Electronics", "Computer",
                        "Biomedecal"]
-        
+
         for i in range(1,number):
             name = random.choice(names)
             program = random.choice(programs)
             level = random.randint(1,4)
             ID = random.randint(2400000, 2499999)
             passw =random.randint(1234567,2345678)
-            
+
             email = (f"{name.lower()}{i}@kau.edu.stu.com")
             user=users_db.add_users((ID,name,email,passw,"student")) 
             user.insertData()
@@ -32,8 +32,7 @@ s= add_student()
 s.createstudent()
 
 # # To test searching for a student by ID
-# id=('2433632')
-# serch_student= users_db.serach([id])
-# student_data=serch_student.fetch_user()
-# print(student_data)
-
+id=('2433632')
+serch_student= users_db.serach([id])
+student_data=serch_student.fetch_user()
+print(student_data)
